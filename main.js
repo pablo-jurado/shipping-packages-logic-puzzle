ec91aa4e01ee6af7b9ebc446cd2eba32cb005c0d
Pablo Jurado <pjurado85@gmail.com>
2017-03-13 19:02:07 -0500

    still working in step 2

--- main.js
+++ main.js
@@ -89,52 +89,41 @@ if (person2.lastName === 'DeForest' && person2.day === 'Saturday') {
 // Connect your data types with your logical statements in order to solve the
 // puzzle. Use console.log to print the answers as sentences like:
 
-
 // 1. Greg’s friend wasn’t Ellen Fairview, who didn’t live in Ohio. The
 // birthday girl didn’t have her party on Friday.
 if (possFirstNames.indexOf('Ellen') !== (undefined || null)) {
-  var nameIndex = possFirstNames.indexOf('Ellen')
-  person1.name = possFirstNames[nameIndex]
-  possFirstNames[nameIndex] = null
-  var lastNameIndex = possLastNames.indexOf('Fairview')
-  person1.lastName = possLastNames[lastNameIndex]
-  possLastNames[lastNameIndex] = null
+  person1.name = possFirstNames[possFirstNames.indexOf('Ellen')]
+  possFirstNames[possFirstNames.indexOf('Ellen')] = null
+  person1.lastName = possLastNames[possLastNames.indexOf('Fairview')]
+  possLastNames[possLastNames.indexOf('Fairview')] = null
   person1.gender = 'She'
 }
 
 // 2. Rick’s last name wasn’t Bartley but his event was on Saturday night.
 if (possFirstNames.indexOf('Rick') !== (undefined || null)) {
-  var nameIndex = possFirstNames.indexOf('Rick')
-  person2.name = possFirstNames[nameIndex]
-  possFirstNames[nameIndex] = null
+  person2.name = possFirstNames[possFirstNames.indexOf('Rick')]
+  possFirstNames[possFirstNames.indexOf('Rick')] = null
 
-  var dayIndex = possDays.indexOf('Saturday')
-  person2.day = possDays[dayIndex]
-  possDays[dayIndex] = null
+  person2.day = possDays[possDays.indexOf('Saturday')]
+  possDays[possDays.indexOf('Saturday')] = null
   person2.gender = 'He'
-
 }
 
 // 3. Greg’s father wasn’t getting married, but his last name was Gray.
 if (possLastNames.indexOf('Gray') !== (undefined || null)) {
-  var lastNameIndex = possLastNames.indexOf('Gray')
-  person3.lastName = possLastNames[lastNameIndex]
-  possLastNames[lastNameIndex] = null
-  var relationIndex = possRelations.indexOf('father')
-  person3.relationship = possRelations[relationIndex]
-  possRelations[relationIndex] = null
+  person3.lastName = possLastNames[possLastNames.indexOf('Gray')]
+  possLastNames[possLastNames.indexOf('Gray')] = null
+  person3.relationship = possRelations[possRelations.indexOf('father')]
+  possRelations[possRelations.indexOf('father')] = null
   person3.gender = 'He'
-
 }
 
 // 4. The friend having a house warming didn’t live in Ohio.
 if (person1.relationship !== 'friend' && person3.relationship === 'father') {
-  var indexRelation = possRelations.indexOf('friend');
-  person2.relationship = possRelations[indexRelation]
-  possRelations[indexRelation] = null
-  var indexEvent = possEvents.indexOf('house warming')
-  person2.event = possEvents[indexEvent]
-  possEvents[indexEvent] = null
+  person2.relationship = possRelations[possRelations.indexOf('friend')]
+  possRelations[possRelations.indexOf('friend')] = null
+  person2.event = possEvents[possEvents.indexOf('house warming')]
+  possEvents[possEvents.indexOf('house warming')] = null
 }
 
 // 5. The wedding was for Greg’s cousin.
@@ -142,45 +131,90 @@ if (person1.relationship !== 'friend' && person3.relationship === 'father') {
 // Greg’s sister but her event wasn’t on Wednesday night.
 
 if (person1.name === 'Ellen') {
-  var indexRelation = possRelations.indexOf('cousin')
-  person1.relationship = possRelations[indexRelation]
-  possRelations[indexRelation] = null
-  var indexEvent = possEvents.indexOf('wedding')
-  person1.event = possEvents[indexEvent]
-  possEvents[indexEvent] = null
+  person1.relationship = possRelations[possRelations.indexOf('cousin')]
+  possRelations[possRelations.indexOf('cousin')] = null
+  person1.event = possEvents[possEvents.indexOf('wedding')]
+  possEvents[possEvents.indexOf('wedding')] = null
 }
 
-if (person4.state !== 'Texas' ) {
-  var nameIndex = possFirstNames.indexOf('Heather')
-  person4.name = possFirstNames[nameIndex]
-  possFirstNames[nameIndex] = null
+if (person4.state !== 'Texas') {
+  person4.name = possFirstNames[possFirstNames.indexOf('Heather')]
+  possFirstNames[possFirstNames.indexOf('Heather')] = null
 
-  var indexRelation = possRelations.indexOf('sister')
-  person4.relationship = possRelations[indexRelation]
-  possRelations[indexRelation] = null
+  person4.relationship = possRelations[possRelations.indexOf('sister')]
+  possRelations[possRelations.indexOf('sister')] = null
+
+  person4.day = possDays[possDays.indexOf('Thursday')]
+  possDays[possDays.indexOf('Thursday')] = null
   person4.gender = 'She'
+
+  for (var i = 0; i < possEvents.length; i++) {
+    if(possEvents[i] === 'birthday') {
+      person4.event = possEvents[i]
+      possEvents[i] = null
+    }
+  }
 }
 
 // 6. Walter’s event was one day earlier than the person whose last name was
 // DeForest but after the person who lived in Washington.
 // The anniversary was held in Montana.
+if ((!person3.name)) {
+  for (var i = 0; i < possFirstNames.length; i++) {
+    if (possFirstNames[i] !== null) {
+      person3.name = possFirstNames[i]
+      possFirstNames[i] = null
+    }
+  }
+}
 
-function logPerson(person) {
+for (var j = 0; j < possLastNames.length; j++) {
+  if (possLastNames[j] === 'DeForest') {
+    person2.lastName = possLastNames[j]
+    possLastNames[j] = null
+  }
+}
+
+person3.event = possEvents[possEvents.indexOf('anniversary')]
+possEvents[possEvents.indexOf('anniversary')] = null
+person3.state = possStates[possStates.indexOf('Montana')]
+possStates[possStates.indexOf('Montana')] = null
+person3.day = possDays[possDays.indexOf('Friday')]
+possDays[possDays.indexOf('Friday')] = null
+
+for (var k = 0; k < possLastNames.length; k++) {
+  if (possLastNames[k] !== null) {
+    person4.lastName = possLastNames[k]
+    possLastNames[k] = null
+  }
+}
+
+logAllPersons()
+logData()
+
+function logPerson (person) {
   console.log(
     person.name + ' ' + person.lastName + ' lives in ' + person.state +
     ' and is Greg\'s ' + person.relationship + '. ' + person.gender + ' had a ' +
-    person.event + ' on ' + person.day +  '.'
-  );
+    person.event + ' on ' + person.day + '.'
+  )
 }
 
-console.log('FirstNames', possFirstNames)
-console.log('LastNames', possLastNames)
-console.log('Relation', possRelations)
-console.log('States', possStates)
-console.log('Events', possEvents)
-console.log('Days', possDays)
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-logPerson(person1)
-logPerson(person2)
-logPerson(person3)
-logPerson(person4)
+function logAllPersons () {
+  logPerson(person1)
+  logPerson(person2)
+  logPerson(person3)
+  logPerson(person4)
+}
+
+function logData () {
+  console.log('FirstNames', possFirstNames)
+  console.log('LastNames', possLastNames)
+  console.log('Relation', possRelations)
+  console.log('States', possStates)
+  console.log('Events', possEvents)
+  console.log('Days', possDays)
+}
+
+// logAllPersons()
+// logData()

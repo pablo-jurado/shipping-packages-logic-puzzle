c8e2cfd8f2ee539234352dd2984cbcab2caf43a3
Pablo Jurado <pjurado85@gmail.com>
2017-03-18 15:06:30 -0500

    I have the data setup, now I need to work on filtering options

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 // Model the data that the puzzle is made out of. What data type(s) can you use
 // to represent this data? Arrays, strings, booleans, objects, etc.
 
-var name = ['Ellen', 'Heather', 'Rick', 'Walter'],
-    lastName = ['Bartley', 'DeForest', 'Fairview', 'Gray'],
-    state = ['Ohio', 'Montana', 'Texas', 'Washington'],
-    day = ['Wednesday', 'Thursday', 'Friday', 'Saturday'],
-    events = ['anniversary', 'birthday', 'house warming', 'wedding']
+var names = ['Ellen', 'Heather', 'Rick', 'Walter'],
+  lastNames = ['Bartley', 'DeForest', 'Fairview', 'Gray'],
+  states = ['Ohio', 'Montana', 'Texas', 'Washington'],
+  days = ['Wednesday', 'Thursday', 'Friday', 'Saturday'],
+  relations = ['friend', 'causin', 'father', 'sister'],
+  genders = ['She', 'He'],
+  events = ['anniversary', 'birthday', 'house warming', 'wedding']
 
 // Step #2
 // What logical statements can you use to model the six rules of the puzzle?
@@ -14,20 +16,59 @@ var name = ['Ellen', 'Heather', 'Rick', 'Walter'],
 // on the logic of the statements.
 
 function createAllOptions () {
+  var persons = []
+  for (var i = 0; i < names.length; i++) {
+    for (var j = 0; j < lastNames.length; j++) {
+      for (var k = 0; k < states.length; k++) {
+        for (var l = 0; l < days.length; l++) {
+          for (var m = 0; m < relations.length; m++) {
+            for (var n = 0; n < genders.length; n++) {
+              for (var o = 0; o < events.length; o++) {
+                persons.push({
+                  name: names[i],
+                  lastName: lastNames[j],
+                  state: states[k],
+                  day: days[l],
+                  relation: relations[m],
+                  gender: genders[n],
+                  events: events[o]
+                })
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return persons
+}
+var allOptions = createAllOptions()
 
+// console.log(allOptions)
+
+function isValidOption (person) {
+  // 1. Greg’s friend wasn’t Ellen Fairview, who didn’t live in Ohio.
+  // The birthday girl didn’t have her party on Friday.
+  if (person.relation === 'friend' && person.name !== 'Ellen' &&
+  person.lastName !== 'Fairview') {
+    return true
+  } else if (person.name === 'Ellen' && person.lastName === 'Fairview' &&
+  person.state !== 'Ohio' && person.gender === 'She') {
+    return true
+  } else {
+    return false
+  }
 }
 
-function createAllSolutions () {
-
+for (var i = 0; i < allOptions.length; i++) {
+  if (isValidOption(allOptions[i])) {
+    console.log(allOptions[i])
+  }
 }
 
-function isValidOption (option) {
-
-}
-
-function isValidSolution (week) {
-
-}
+// function isValidSolution (solution) {
+//
+// }
 
 // 1. Greg’s friend wasn’t Ellen Fairview, who didn’t live in Ohio.
 // The birthday girl didn’t have her party on Friday.

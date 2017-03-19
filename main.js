cabd93594a27d8f1a7b436e5d98669cac6a373b8
Pablo Jurado <pjurado85@gmail.com>
2017-03-19 15:59:53 -0500

    got down to 7 options, still need to work on #6

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 // Model the data that the puzzle is made out of. What data type(s) can you use
 // to represent this data? Arrays, strings, booleans, objects, etc.
 
-var names = ['Ellen', 'Heather', 'Rick', 'Walter'],
-  lastNames = ['Bartley', 'DeForest', 'Fairview', 'Gray'],
-  states = ['Ohio', 'Montana', 'Texas', 'Washington'],
-  days = ['Wednesday', 'Thursday', 'Friday', 'Saturday'],
-  relations = ['friend', 'cousin', 'father', 'sister'],
-  genders = ['She', 'He'],
-  events = ['anniversary', 'birthday', 'house warming', 'wedding']
+var names = ['Ellen', 'Heather', 'Rick', 'Walter']
+var lastNames = ['Bartley', 'DeForest', 'Fairview', 'Gray']
+var states = ['Ohio', 'Montana', 'Texas', 'Washington']
+var days = ['Wednesday', 'Thursday', 'Friday', 'Saturday']
+var relations = ['friend', 'cousin', 'father', 'sister']
+var genders = ['She', 'He']
+var events = ['anniversary', 'birthday', 'house warming', 'wedding']
 
 // Step #2
 // What logical statements can you use to model the six rules of the puzzle?
@@ -49,35 +49,75 @@ var allOptions = createAllOptions()
 function isValidOption (person) {
   // 1. Greg’s friend wasn’t Ellen Fairview, who didn’t live in Ohio.
   // 4. The friend having a house warming didn’t live in Ohio.
-  if (person.relation === 'friend' && person.name !== 'Ellen' &&
-  person.name !== 'Heather' && person.lastName !== 'Fairview' &&
-  person.lastName !== 'Gray' && person.events === 'house warming' &&
-  person.state !== 'Ohio' && person.state !== 'Montana' && person.gender === 'He') {
-    return true
-  // 5. The wedding was for Greg’s cousin.
-  } else if (person.name === 'Ellen' && person.lastName === 'Fairview' &&
-  person.state !== 'Ohio' && person.relation !== 'friend' &&
-  person.relation !== 'father' && person.relation !== 'sister' &&   // 'causin' 'wedding'
-  person.events === 'wedding' && person.day !== 'Saturday' &&
-  person.state !== 'Montana' && person.gender === 'She') {
-    return true
-  // 2. Rick’s last name wasn’t Bartley but his event was on Saturday night. //
-  } else if (person.name === 'Rick' && person.lastName !== 'Bartley' &&
-  person.lastName !== 'Fairview' && person.day === 'Saturday' &&
+  // 2. Rick’s last name wasn’t Bartley but his event was on Saturday night.
+  if (person.relation === 'friend' &&
+  person.name !== 'Ellen' &&
+  person.name !== 'Heather' &&
+  person.name === 'Rick' &&
+  person.lastName !== 'Fairview' &&
+  person.lastName !== 'Gray' &&
+  person.lastName !== 'Bartley' &&
+  person.lastName === 'DeForest' &&
+  person.state !== 'Ohio' &&
+  person.state !== 'Montana' &&
+  person.day !== 'Thursday' &&
+  person.day === 'Saturday' &&
+  person.events !== 'wedding' &&
+  person.events !== 'birthday' &&
+  person.events === 'house warming' &&
+  person.state !== 'Washington' &&   // DeForest but after the person who lived in Washington.
   person.gender === 'He') {
     return true
+  // 1. Ellen Fairview, who didn’t live in Ohio.
+  // 5. The wedding was for Greg’s cousin.
+  } else if (person.name === 'Ellen' &&
+  person.lastName === 'Fairview' &&
+  person.relation !== 'friend' &&
+  person.relation !== 'father' &&
+  person.relation !== 'sister' &&   // 'causin' 'wedding'
+  person.events === 'wedding' &&
+  person.day !== 'Saturday' &&
+  person.day !== 'Thursday' &&
+  person.state !== 'Ohio' &&
+  person.state !== 'Montana' &&
+  person.state !== 'Texas' &&
+  person.gender === 'She') {
+    return true
+  // 2. Rick’s last name wasn’t Bartley but his event was on Saturday night. //
+  // } else if (person.name === 'Rick' && person.lastName !== 'Bartley' &&
+  // person.lastName !== 'Fairview' && person.day === 'Saturday' &&
+  // person.events !== 'wedding' && person.events !== 'birthday' &&
+  // person.gender === 'He' && person.lastName === 'DeForest') {
+  //   return true
+
   // 3. Greg’s father wasn’t getting married, but his last name was Gray.
-  } else if (person.relation === 'father' && person.events !== 'wedding' &&
-  person.lastName === 'Gray' && person.name !== 'Ellen' &&
-  person.name !== 'Heather' && person.gender === 'He') {
+  // The anniversary was held in Montana.
+  } else if (person.relation === 'father' &&
+  person.events !== 'wedding' &&
+  person.lastName === 'Gray' &&
+  person.name !== 'Ellen' &&
+  person.name !== 'Heather' &&
+  person.name === 'Walter' &&
+  person.day !== 'Thursday' &&
+  person.day !== 'Saturday' &&
+  person.gender === 'He' &&
+  person.events === 'anniversary' &&
+  person.state === 'Montana') {
     return true
   // The birthday girl didn’t have her party on Friday.
   // Heather, who didn’t live in Texas, was
   // Greg’s sister but her event wasn’t on Wednesday night.
-  } else if (person.name === 'Heather' && person.state !== 'Texas' &&
-  person.state !== 'Montana' && person.relation === 'sister' && person.day !== 'Wednesday' &&
-  person.lastName === 'Bartley' && person.events === 'birthday' && // 2. Rick’s last name wasn’t Bartley
-  person.day !== 'Saturday' && person.gender === 'She') {
+  } else if (person.name === 'Heather' &&
+  person.state !== 'Texas' &&
+  person.state !== 'Montana' &&
+  person.relation === 'sister' &&
+  person.lastName === 'Bartley' &&
+  person.events === 'birthday' && // 2. Rick’s last name wasn’t Bartley
+  person.day !== 'Wednesday' &&
+  person.day !== 'Friday' && // heather Bartley sister birthday Thursday
+  person.day !== 'Saturday' &&
+  person.day === 'Thursday' &&
+  person.gender === 'She') {
     return true
   // 6. Walter’s event was one day earlier than the person whose last name was
   // DeForest but after the person who lived in Washington.
